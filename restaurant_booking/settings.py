84e236425de4bfced43dee0ebc80c6683cb5cc5c
Özlem Hafalır <ozlemoztep@gmail.com>
2023-11-07 20:59:16 +0100

    Redirect to homepage after login

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -103,6 +103,7 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+LOGIN_REDIRECT_URL = '/'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
